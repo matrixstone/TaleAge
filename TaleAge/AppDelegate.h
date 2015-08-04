266a2daf33f9e2b2a56630298bd399719371f1f3
xuhe <xuhe@yahoo-inc.com>
2015-08-04 14:52:58 -0700

    Initial Commit

---
+++ TaleAge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaleAge
+//
+//  Created by Xu He on 8/4/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaleAge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaleAge
+//
+//  Created by Xu He on 8/4/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
